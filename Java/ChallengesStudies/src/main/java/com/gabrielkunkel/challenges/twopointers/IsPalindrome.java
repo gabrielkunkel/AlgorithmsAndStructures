c60cb0142b942954b2e7835e35449b2dc8bdeeff
Gabriel Kunkel <henrysaltandson@gmail.com>
2023-12-29 11:34:27 -0600

    add tighter IsPalindrome solution

--- Java/ChallengesStudies/src/main/java/com/gabrielkunkel/challenges/twopointers/IsPalindrome.java
+++ Java/ChallengesStudies/src/main/java/com/gabrielkunkel/challenges/twopointers/IsPalindrome.java
@@ -33,5 +33,31 @@ public class IsPalindrome {
         return true;
     }
 
+    public boolean tighter(String s) {
+        if (s.isEmpty()) {
+            return true;
+        }
+        int start = 0;
+        int last = s.length() - 1;
+        while(start <= last) {
+            char currFirst = s.charAt(start);
+            char currLast = s.charAt(last);
+            if (!Character.isLetterOrDigit(currFirst )) {
+                start++;
+            } else if(!Character.isLetterOrDigit(currLast)) {
+                last--;
+            } else {
+                if (Character.toLowerCase(currFirst) != Character.toLowerCase(currLast)) {
+                    return false;
+                }
+                start++;
+                last--;
+            }
+        }
+        return true;
+
+
+    }
+
 
 }
