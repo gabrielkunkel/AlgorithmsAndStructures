030f068a2d28c72e799f3414fa199ff46b0ede01
Gabriel Kunkel <henrysaltandson@gmail.com>
2023-12-30 13:44:51 -0600

    add isPalindrome solution

---
+++ Python/challengesStudies/TwoPointers/is_palindrome.py
@@ -0,0 +1,17 @@
+class IsPalindrome:
+    def isPalindrome(self, s: str) -> bool:
+        left: int = 0
+        right: int = len(s) - 1
+        while left < right:
+            if not (s[left].isdigit() or s[left].isalpha()):
+                left += 1
+                continue
+            elif not (s[right].isdigit() or s[right].isalpha()):
+                right -= 1
+                continue
+            elif s[left].lower() != s[right].lower():
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
