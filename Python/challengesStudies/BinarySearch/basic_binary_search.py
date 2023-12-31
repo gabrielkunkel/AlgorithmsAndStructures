38af9e51fc95abb4da75df6ad0871ce6f2e2606b
Gabriel Kunkel <henrysaltandson@gmail.com>
2023-12-31 12:58:26 -0600

    add bad version, slower performance study

--- Python/challengesStudies/BinarySearch/basic_binary_search.py
+++ Python/challengesStudies/BinarySearch/basic_binary_search.py
@@ -19,3 +19,29 @@ class BasicBinarySearch:
                 return midpoint
 
         return -1
+
+    @staticmethod
+    def slower_wrong_incrementing(nums: List[int], target: int) -> int:
+        """
+            This version of binary search is slower and problematic...
+            left and right boundaries are adjusted within the loop. In standard binary
+            search, you narrow down the search space by half after each comparison,
+            which is what gives it O(log n) time complexity.
+        :param nums:
+        :param target:
+        :return:
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while left <= right:
+            mid = (right + left) // 2
+
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = left + 1  # should be midpoint + 1
+            else:
+                right = right - 1  # should be midpoint - 1
+
+        return -1
